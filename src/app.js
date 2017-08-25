50425f7488a64910ab75fde0698c8f1933c6696e
Chotipat Top <topz.classic@gmail.com>
2017-08-25 17:15:40 +0700

    Login and logout firebase
    Success Authentication

--- src/app.js
+++ src/app.js
@@ -6,15 +6,17 @@
 
 import React, { Component } from 'react';
 import {
+  StyleSheet,
   AppRegistry,
   View
 } from 'react-native';
-import {Header} from './components/common'
+import {Header, Button, Spinner} from './components/common'
 import firebase from 'firebase'
 import LoginForm from './components/LoginForm'
 
 export default class AuthApp extends Component {
 
+  state={loggedIn:true}
 
   componentDidMount(){
     firebase.initializeApp({
@@ -25,14 +27,47 @@ export default class AuthApp extends Component {
       storageBucket: 'authentication-fdc93.appspot.com',
       messagingSenderId: '81111653508'
     })
+
+    firebase.auth().onAuthStateChanged((user)=>{
+      if(user){
+        this.setState({loggedIn:true})
+      }else{
+        this.setState({loggedIn:false})
+      }
+    })
+  }
+  renderContent(){
+      switch(this.state.loggedIn){
+          case true:
+              return (
+                  <View style={styles.containerStyle}>  
+                  <Button onPress={()=>firebase.auth().signOut()}>
+                      Log Out
+                  </Button>
+                  </View>
+            )      
+        case false:
+          return <LoginForm/>
+        default:
+          return <Spinner size="large"/>
+    }
   }
   render() {
     return (
       <View>
       <Header headerText="Authentication"/>
-      <LoginForm/>
+        {this.renderContent()}
       </View>
     );
   }
 }
+const styles = StyleSheet.create({
+  containerStyle: {
+      padding: 5,
+      backgroundColor: '#fff',
+      justifyContent: 'flex-start',
+      flexDirection: 'row',
+      position: 'relative',
+  },
+});
 AppRegistry.registerComponent('AuthApp', () => AuthApp);
