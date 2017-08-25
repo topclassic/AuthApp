0b8f4cdd7963d42dd87f9d46901ddbd1ea756e16
Chotipat Top <topz.classic@gmail.com>
2017-08-25 11:04:45 +0700

    Setup Firebase

--- src/app.js
+++ src/app.js
@@ -12,8 +12,21 @@ import {
   View
 } from 'react-native';
 import {Header} from './components/common'
+import firebase from 'firebase'
 
 export default class AuthApp extends Component {
+
+
+  componentDidMount(){
+    firebase.initializeApp({
+      apiKey: 'AIzaSyB7L5KyouyVz8iGAOAlKZjSuBFc5pZ-47M',
+      authDomain: 'authentication-fdc93.firebaseapp.com',
+      databaseURL: 'https://authentication-fdc93.firebaseio.com',
+      projectId: 'authentication-fdc93',
+      storageBucket: 'authentication-fdc93.appspot.com',
+      messagingSenderId: '81111653508'
+    })
+  }
   render() {
     return (
       <View>
